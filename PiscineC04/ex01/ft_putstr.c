f3d03f89148c6f032d7d875c955388e6cd33a2fb
FedeV42 <101577887+FedeV42@users.noreply.github.com>
2022-04-07 21:31:49 +0200

    Add files via upload
    C04

---
+++ PiscineC04/ex01/ft_putstr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cvazquez <cvazquez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/06 19:07:44 by cvazquez          #+#    #+#             */
+/*   Updated: 2022/04/06 19:07:45 by cvazquez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+/*
+int	main(void)
+{
+	char str[] = "abcdefg";
+	ft_putstr(str);
+	return (0);
+}*/
---
+++ PiscineC04/ex02/ft_putnbr.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cvazquez <cvazquez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/06 19:07:03 by cvazquez          #+#    #+#             */
+/*   Updated: 2022/04/07 20:28:15 by cvazquez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+	}
+	else if (nb < 0)
+	{
+		nb = -nb;
+		ft_putchar('-');
+		ft_putnbr(nb);
+	}
+	else if (nb < 10)
+	{
+		ft_putchar(nb + '0');
+	}
+	else
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar(nb % 10 + '0');
+	}
+}
+/*
+int	main(void)
+{
+	ft_putnbr(2147483647);
+	return (0);
+}*/
